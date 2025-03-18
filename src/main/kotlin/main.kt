c0c7bdf2186ae949df571e9f7691b8bda71c5a16
Sergey <asanbaevsn@ru-drive.com>
2025-03-18 13:12:30 +0300

    second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,28 @@
 fun main() {
-    val amount = 5000
+    val amount = 10000
     val isRegularCustomer = true
-    val discount = calculationDiscount(amount, isRegularCustomer)
+
+    val basicDiscount = if (amount > 1 && amount < 1000) {     //базовая скидка
+        0
+    } else if (amount > 1001 && amount < 10000) {
+        100
+    } else if (amount >= 10000) {
+        (amount * 0.05).toInt()
+    } else {
+        0
+    }
+
+    val regularDiscount = if (isRegularCustomer) {              //постоянного покупателя скидка
+        ((amount - basicDiscount) / 100).toInt()
+    } else {
+        0
+    }
+
+    val discount = basicDiscount + regularDiscount             //скидка вся
+
     println("Сумма покупки: $amount рублей")
+    println("Сумма базовой скидки: $basicDiscount рублей")
+    println("Скидка постоянного покупателя : $regularDiscount рублей")
     println("Размер скидки: $discount рублей")
-
 }
 
-fun calculationDiscount(amount: Int, isRegularCustomer: Boolean): Int {
-    return when {
-        amount in 1..1000 -> 0
-        amount in 1001..10000 -> 100
-        amount > 10000 -> (amount * 0.05).toInt()
-        else -> 0
-    } + if (isRegularCustomer) (amount / 100).toInt() else 0
-}
